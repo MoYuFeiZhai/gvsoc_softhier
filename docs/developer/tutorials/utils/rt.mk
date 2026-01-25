9d78c5677dc5d368560abaa8184ff20d9c2fdc02
MoYuFeiZhai <innerpeace.rah@gmail.com>
2026-01-25 20:58:04 +0100

    tutorial 0 success

--- docs/developer/tutorials/utils/rt.mk
+++ docs/developer/tutorials/utils/rt.mk
@@ -1,2 +1,5 @@
 RT_SRCS = ../utils/init.c ../utils/crt0.S ../utils/alloc.c ../utils/io.c ../utils/prf.c ../utils/string.c ../utils/fprintf.c
-RT_FLAGS = -march=rv64imafdc -O3 -fno-tree-loop-distribute-patterns -I../utils -T../utils/link.ld -nostartfiles -nostdlib -Wl,--no-warn-rwx-segments
+# Use picolibc headers/libs provided by Ubuntu's riscv64-unknown-elf packages.
+PICO_SYSROOT ?= /usr/lib/picolibc/riscv64-unknown-elf
+RT_FLAGS = -march=rv64imafdc -O3 -fno-tree-loop-distribute-patterns -I../utils -T../utils/link.ld -nostartfiles -nostdlib \
+	--sysroot=$(PICO_SYSROOT) -specs=picolibc.specs
